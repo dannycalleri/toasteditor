5b6b60038bad6042fc90af49a043bcbde5b64564
Danny Calleri <daniele.calleri@gmail.com>
2013-10-10 11:36:42 +0200

    Version 0.1: Now exports for FlashPunk, Toast, C++ and AS3
    Added export support for FlashPunk, Toast, C++ and AS3

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+### HEAD
+
+### 0.1 (October 8, 2013)
+
+* Added export support for FlashPunk, Toast Framework
+* Added export support for C++/AS3 using a simple array export
+* Tools updated with eraser
+* Windows can now be dragged using jQuery UI
---
+++ js/dragdroploader.js
@@ -0,0 +1,106 @@
+// The following code is taken from a great blog post
+// by Robert Gravelle. You can find it here:
+// URL = http://www.htmlgoodies.com/html5/javascript/drag-files-into-the-browser-from-the-desktop-HTML5.html
+
+var tilesetLoaded = false;
+
+if(window.FileReader)
+{
+    var drop;
+    
+    addEventHandler(window, 'load', function() {
+        
+        var status = document.getElementById('status');
+        drop = document.getElementById('drop');
+        var list = document.getElementById('list');
+
+        function cancel(e) 
+        {
+            if (e.preventDefault) { e.preventDefault(); }
+            return false;
+        }
+
+        // Tells the browser that we *can* drop on this target
+        addEventHandler(drop, 'dragover', cancel);
+        addEventHandler(drop, 'dragenter', cancel);
+
+        addEventHandler(drop, 'drop', function (e) {
+            e = e || window.event; // get window.event if e argument missing (in IE)   
+            if (e.preventDefault) { e.preventDefault(); } // stops the browser from redirecting off to the image.
+
+            var dt    = e.dataTransfer;
+            var files = dt.files;
+
+            for (var i=0; i < files.length; i++)
+            {
+                var file = files[i];
+                var reader = new FileReader();
+
+                //attach event handlers here...
+
+                reader.readAsDataURL(file);
+                
+                addEventHandler(reader, 'loadend', function(e, file) {
+                    var bin           = this.result; 
+                    var newFile       = document.createElement('div');
+                    newFile.innerHTML = 'Loaded : '+file.name+' size '+file.size+' B';
+
+                    list.appendChild(newFile);  
+                    
+                    var fileNumber = list.getElementsByTagName('div').length;
+                    status.innerHTML = fileNumber < files.length 
+                                 ? 'Loaded 100% of file '+fileNumber+' of '+files.length+'...' 
+                                 : 'Done loading. processed '+fileNumber+' files.';
+
+                    var img = document.createElement("img"); 
+                    img.setAttribute("id", "tileset");
+                    img.file = file;
+                    img.src = bin;
+                    document.getElementById("tileset-container").appendChild(img);
+
+                    tilesetLoaded=true;
+
+                    //list.appendChild(img);
+                }.bindToEventHandler(file));
+            }
+            
+            return false;
+        });
+
+        Function.prototype.bindToEventHandler = function bindToEventHandler() 
+        {
+            var handler = this;
+            var boundParameters = Array.prototype.slice.call(arguments);
+            //create closure
+            return function(e) 
+            {
+                e = e || window.event; // get window.event if e argument missing (in IE)   
+                boundParameters.unshift(e);
+                handler.apply(this, boundParameters);
+            }
+        };
+    });
+} 
+else 
+{ 
+    document.getElementById('status').innerHTML = 'Your browser does not support the HTML5 FileReader.';
+}
+
+function addEventHandler(obj, evt, handler) 
+{
+    if(obj.addEventListener) 
+    {
+        // W3C method
+        obj.addEventListener(evt, handler, false);
+    } 
+    else if(obj.attachEvent) 
+    {
+        // IE method.
+        obj.attachEvent('on'+evt, handler);
+    } 
+    else 
+    {
+        // Old school method.
+        obj['on'+evt] = handler;
+    }
+}
